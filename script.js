0869f983507db9f706351ce369c0d4191f879bcf
BaxtiyorXusanov <xusanovbaxtiyor07>
2021-09-17 21:12:41 +0500

    add some changes

--- script.js
+++ script.js
@@ -5,42 +5,52 @@ let elGreen = document.querySelector('#green');
 let elResultPlace = document.querySelector('#resultplace');
 let elWrapper = document.querySelector('.wrapper')
 let elBtn = document.querySelector('#btn')
+let elSubTitle = document.querySelector('#subtitle')
 
 elBtn.addEventListener('click', function(){
     rec.start()
+
+    elSubTitle.textContent = ''
     
     // rec.lang = 'en-US';
     
     rec.onerror = function(e) {
+        elYellow.style.background = ''
+        elGreen.style.background = ''
+        elRed.style.background = ''
         elResultPlace.textContent = 'Qaysi rangni hohlaysiz'
     }
     
     rec.onend = function(e){
-        console.log('aloqa tugadi')
+        elSubTitle.textContent = 'Aloqa tugadi'
     }
     
     rec.onresult = function(e){
-        if(e.results[0][0].transcript == 'красный' && 'qizil' && 'krasniy'){
+        if(e.results[0][0].transcript == 'красный' && 'qizil'){
             elGreen.style.background = ''
             elYellow.style.background = ''
             elRed.style.background = 'red'
             elResultPlace.textContent = e.results[0][0].transcript
             elResultPlace.style.borderBottom = '2px solid red'
             elWrapper.style.boxShadow = '5px 5px 5px red, -5px -5px 5px red, 5px -5px 5px red, -5px 5px 5px red'
-        }else if(e.results[0][0].transcript == 'жёлтый' && 'joltiy' && 'sariq'){
+        }else if(e.results[0][0].transcript == 'жёлтый' && 'sariq' ){
             elRed.style.background = ''
             elGreen.style.background = ''
             elYellow.style.background = 'gold'
             elResultPlace.textContent = e.results[0][0].transcript
             elResultPlace.style.borderBottom = '2px solid yellow'
             elWrapper.style.boxShadow = '5px 5px 5px yellow, -5px -5px 5px yellow, 5px -5px 5px yellow, -5px 5px 5px yellow'
-        }else if(e.results[0][0].transcript == 'зелёный' && 'zelyoniy' && 'yashil'){
+        }else if(e.results[0][0].transcript == 'зелёный' && 'yashil'){
             elYellow.style.background = ''
+            elRed.style.background = ''
             elResultPlace.textContent = e.results[0][0].transcript
             elResultPlace.style.borderBottom = '2px solid green'
             elWrapper.style.boxShadow = '5px 5px 5px green, -5px -5px 5px green, 5px -5px 5px green, -5px 5px 5px green'
             elGreen.style.background = 'green'
         }else{
+            elYellow.style.background = ''
+            elGreen.style.background = ''
+            elRed.style.background = ''
             elResultPlace.textContent = e.results[0][0].transcript
         }
     }
